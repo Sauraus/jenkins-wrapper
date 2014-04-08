746c10f7a22a53c5d367f72f03bf5d581edac23c
Antek Baranski <antek.baranski@disney.com>
2014-04-08 14:51:47 -0700

    cleanup metadata and bump version

--- metadata.rb
+++ metadata.rb
@@ -4,13 +4,9 @@ maintainer_email '#DI CP DevOps Engineers'
 license          'Apache 2.0'
 description      'Installs and configures Jenkins CI master & slaves'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.1'
+version          '0.0.2'
 
 recipe 'diw-jenkins', 'Installs a Jenkins master'
 
-#depends 'twdc_chef_handler'
-#depends 'twdc_windows_task'
-#depends 'twdc_chef_node'
-#depends 'twdc_chef-client'
 depends 'java'
 depends 'jenkins'
